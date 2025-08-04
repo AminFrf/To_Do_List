cb8a0094f27fbacfcc7148823f5021cba5254a11
Notebook <mhmd.amin.faridfar@gmail.com>
2025-08-05 02:01:26 +0330

    working on the functions and main menu

--- TO_DO_LIST.py
+++ TO_DO_LIST.py
@@ -1,9 +1,10 @@
 class task :
+    
     def __init__(self , name , description , priority) :
         self.name = name
         self.description = description
         self.priority = priority
-
+        
 
 
 class to_do_list :
@@ -13,12 +14,26 @@ class to_do_list :
     
     
     def main_menu(self) :
-        print("*** TO DO LIST ***")
-        print("1. add new task")
-        print("2. delete task")
-        print("3. see list of tasks")
-        print("4. save list")
-        print("enter your chois : ")
+        
+        print_again = True
+        choice = 0 
+        
+        while(print_again) :
+            
+            print("*** TO DO LIST ***")
+            print("1. add new task")
+            print("2. delete task")
+            print("3. see list of tasks")
+            print("4. save list")
+            print("5. exite")
+            choice = input("enter your chois : ")
+            
+            if choice not in ["1" , "2" ,"3" ,"4" ,"5" ]:
+                print("wrong input !!!")
+            else : 
+                print_again = False
+        return choice 
+        
     
     def add_task(self):
         name = input("pleas enter a title for your : ")
@@ -34,6 +49,38 @@ class to_do_list :
         new_task = task(name , description , priority)     
         self.todolist.append(new_task)   
             
-    
+    def delete_task(self) :
+        name = input("enter the name of the task you want to delete")
+        find = False
+        for t in self.todolist :
+            if t.name == name :
+                self.todolist.remove(t)
+                print(f"task {name} deleted successfully")
+                find = True
+                break        
         
-#start
+        if find == False :
+            print(f"task {name} not found")    
+            
+    def see_list_of_tasks(self) :
+        
+        if not self.todolist :
+            print("no task to show !!!")
+            return
+        
+        for t in self.todolist :
+            
+            print("name : " + t.name)
+            print("description : " + t.description)
+            print("priority : " + t.priority)
+            print("******************************************")
+            
+            
+if __name__ == "__main__" :
+    
+    my_todo_list = to_do_list([])
+    running = True
+    
+    while(running) :
+        
+        my_todo_list.main_menu()
